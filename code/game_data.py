3c231dffc156340a9e1a0c86b7fc36417c31d534
Miguel <miguel.santoss085@gmail.com>
2023-12-01 17:25:03 -0300

    Mapa e inimigos adicionados

---
+++ code/game_data.py
@@ -0,0 +1,10 @@
+level_0 = {
+		'estrutura':'levels/0/level_0_estrutura.csv',
+		'moedas':'levels/0/level_0_moedas.csv',
+		'caixote':'levels/0/level_0_caixotes.csv',
+		'enemies':'levels/0/level_0_enemies.csv',
+		'limite':'levels/0/level_0_limite.csv',
+		'player':'levels/0/level_0_player.csv',
+		'espinhos':'levels/0/level_0_espinhos.csv',
+        'cranio':'levels/0/level_0_cranio.csv'
+        }
